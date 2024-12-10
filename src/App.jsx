93e81fe3bca487d726d18ee42c93b8afc02c4245
Ama <miriams@missionreadyhq.com>
2024-12-10 20:19:38 +1300

    added an h3

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ function App() {
     <>
       <h1>This will be deployed</h1>
       <h2 style={{backgroundColor: "black", color: "aqua"}}>This has been added after the Netlify deploy</h2>
+      <h3 style={{backgroundColor: "aqua", color: "black"}}>This is now also deployed on Vercel</h3>
     </>
   )
 }
