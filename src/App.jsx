c9031dd519a8917559627b995615dce90e867976
Ama <miriams@missionreadyhq.com>
2024-12-10 20:01:20 +1300

    added an h2

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ function App() {
   return (
     <>
       <h1>This will be deployed</h1>
+      <h2 style={{backgroundColor: "aqua", color: "deepskyblue"}}>This has been added after the Nwetlify ddeploy</h2>
     </>
   )
 }
