74243bd757028c561268a3116beba4a89d9f453b
Ama <miriams@missionreadyhq.com>
2024-12-10 20:03:23 +1300

    changed text colour

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <>
       <h1>This will be deployed</h1>
-      <h2 style={{backgroundColor: "aqua", color: "deepskyblue"}}>This has been added after the Nwetlify ddeploy</h2>
+      <h2 style={{backgroundColor: "aqua", color: "black"}}>This has been added after the Nwetlify ddeploy</h2>
     </>
   )
 }
